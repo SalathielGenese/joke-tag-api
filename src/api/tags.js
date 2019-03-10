bff26e87607d3236c1c0d0ce544eb5237cef3316
SalathielGenese <salathielgenese@gmail.com>
2019-03-10 15:03:41 +0100

    merge API into single file & FIX operationId naming

--- src/api/tags.js
+++ src/api/tags.js
@@ -1,4 +1,4 @@
-import { sequelize } from '../models/index';
+import { sequelize } from '../../models/index';
 import debug from 'debug';
 
 
@@ -9,7 +9,7 @@ import debug from 'debug';
  *   get:
  *     tags: [ "Tags" ]
  *     summary: Tags list
- *     operationId: get_tags
+ *     operationId: tags_get_tags
  *     description: List all tags
  *     responses:
  *       200:
@@ -39,7 +39,7 @@ export const get_tags = ( request, response ) =>
  *   post:
  *     tags: [ "Tags" ]
  *     summary: Tags list
- *     operationId: post_tags
+ *     operationId: tags_post_tags
  *     description: Create tag
  *     parameters:
  *     - in: body
@@ -62,7 +62,9 @@ export const get_tags = ( request, response ) =>
  */
 export const post_tags = ( request, response ) =>
 {
-    const label = request.swagger.params.label.value;
+    const label = request.body.label.value;
+    console.log({ label });
+
 
     return sequelize.model( 'Tag' ).findAll({ where: { label } }).then( tags =>
     {
@@ -89,6 +91,80 @@ export const post_tags = ( request, response ) =>
                 response.json({ content, status: 'success' });
             });
         }
+    }).catch( reason =>
+    {
+        logerror( reason );
+        throw reason;
+    });
+};
+
+/**
+ * @swagger
+ * /tag/{id}:
+ *   get:
+ *     tags: [ "Tags" ]
+ *     summary: Tags list
+ *     operationId: tags_get_tag
+ *     description: Get tag by its ID
+ *     parameters:
+ *     - in: path
+ *       name: id
+ *       type: number
+ *       required: true
+ *     responses:
+ *       200:
+ *         $ref: '#/responses/TagRet'
+ *       500:
+ *         $ref: '#/responses/serverErrorResponse'
+ *
+ *
+ * @param { import( 'swagger-tools' ).Swagger20Request } request
+ * @param { import( 'swagger-tools' ).Swagger20Response } response
+ *
+ */
+export const get_tag = ( request, response ) =>
+{
+    return sequelize.model( 'Tag' ).findById( request.swagger.params.id.value ).then( content =>
+    {
+        response.json({ content, status: 'sucess' });
+    }).catch( reason =>
+    {
+        logerror( reason );
+        throw reason;
+    });
+};
+
+/**
+ * @swagger
+ * /tag/{id}:
+ *   delete:
+ *     tags: [ "Tags" ]
+ *     summary: Tags list
+ *     operationId: tags_delete_tag
+ *     description: Delete tag by its ID
+ *     parameters:
+ *     - in: path
+ *       name: id
+ *       type: number
+ *       required: true
+ *     responses:
+ *       200:
+ *         $ref: '#/responses/TagDeleteRet'
+ *       500:
+ *         $ref: '#/responses/serverErrorResponse'
+ *
+ *
+ * @param { import( 'swagger-tools' ).Swagger20Request } request
+ * @param { import( 'swagger-tools' ).Swagger20Response } response
+ *
+ */
+export const delete_tag = ( request, response ) =>
+{
+    const id = request.swagger.params.id.value;
+
+    return sequelize.model( 'Tag' ).destroy({ where: { id } }).then( () =>
+    {
+        response.json({ content: id, status: 'sucess' });
     }).catch( reason =>
     {
         logerror( reason );
