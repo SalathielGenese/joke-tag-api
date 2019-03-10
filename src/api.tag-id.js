4ee1e9a1ed84fd92f66fa285aca574a90c491d8c
SalathielGenese <salathielgenese@gmail.com>
2019-03-10 10:43:05 +0100

    implements DELETE /tag/{id}

--- src/api.tag-id.js
+++ src/api.tag-id.js
@@ -29,9 +29,47 @@ import debug from 'debug';
  */
 export const get_tag_by_id = ( request, response ) =>
 {
-    sequelize.model( 'Tag' ).findById( request.swagger.params.id.value ).then( content =>
+    return sequelize.model( 'Tag' ).findById( request.swagger.params.id.value ).then( content =>
     {
         response.json({ content, status: 'sucess' });
+    }).catch( reason =>
+    {
+        logerror( reason );
+        throw reason;
+    });
+};
+
+/**
+ * @swagger
+ * /tag/{id}:
+ *   delete:
+ *     tags: [ "Tags" ]
+ *     summary: Tags list
+ *     operationId: delete_tag_by_id
+ *     description: Delete tag by its ID
+ *     parameters:
+ *     - in: path
+ *       name: id
+ *       type: number
+ *       required: true
+ *     responses:
+ *       200:
+ *         $ref: '#/responses/TagDeleteRet'
+ *       500:
+ *         $ref: '#/responses/serverErrorResponse'
+ *
+ *
+ * @param { import( 'swagger-tools' ).Swagger20Request } request
+ * @param { import( 'swagger-tools' ).Swagger20Response } response
+ *
+ */
+export const delete_tag_by_id = ( request, response ) =>
+{
+    const id = request.swagger.params.id.value;
+
+    return sequelize.model( 'Tag' ).destroy({ where: { id } }).then( () =>
+    {
+        response.json({ content: id, status: 'sucess' });
     }).catch( reason =>
     {
         logerror( reason );
