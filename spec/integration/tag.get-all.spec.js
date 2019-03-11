3dc13756df150714395334fe2d981661731f96c8
SalathielGenese <salathielgenese@gmail.com>
2019-03-11 21:56:55 +0100

    spec that GET /tags return all exisiting tags

--- spec/integration/tag.get-all.spec.js
+++ spec/integration/tag.get-all.spec.js
@@ -1,9 +1,13 @@
+import { sequelize } from '../../models/index';
 import { endpoint } from '../endpoint';
+import uuid from 'uuid/v4';
 
 
 
 describe( 'GET /tags', () =>
 {
+
+    const Tag = sequelize.model( 'Tag' );
 
     describe( 'path', () =>
     {
@@ -48,6 +52,20 @@ describe( 'GET /tags', () =>
             it( '.content as Array', () =>
             {
                 expect( Array.isArray( response.body.content ) ).toBe( true );
+            });
+
+            it( '.content has all entities', async () =>
+            {
+                const sometags = [];
+                for ( let i = 0; i < 10; i++ )
+                    sometags.push( ( await Tag.create({ label: uuid.v4() }) ).dataValues );
+                const tags = ( await endpoint.api.get( '/tags' ) ).body.content;
+                const HAS_ALL_CREATED_TAGS = sometags.every( sometag =>
+                    tags.find( tag => tag.id == sometag.id && tag.label === sometag.label ));
+
+                expect( HAS_ALL_CREATED_TAGS ).toBe( true );
+
+                await Tag.destroy({ where: { id: sometags.map( tag => tag.id ) } });
             });
 
         });
