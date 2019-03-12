bb589bc773d2169e0968fd37c04d88b6ade32eb9
SalathielGenese <salathielgenese@gmail.com>
2019-03-12 04:06:56 +0100

    spec DELETE /tag/{id}

---
+++ spec/integration/tag.delete-by-id.spec.js
@@ -0,0 +1,55 @@
+import { sequelize } from '../../models/index';
+import { endpoint } from '../endpoint';
+import uuid from 'uuid';
+
+
+
+describe( 'GET /tag/{id}', () =>
+{
+    beforeEach( async () =>
+    {
+        label = uuid();
+        tag = await Tag.create({ label });
+    });
+
+    afterEach( async () =>
+    {
+        await tag.destroy();
+    })
+
+    let tag;
+    let label;
+    const Tag = sequelize.model( 'Tag' );
+
+    describe( 'response', () =>
+    {
+
+        describe( 'body', () =>
+        {
+
+            it( 'is object', async () =>
+            {
+                const { body } = await endpoint.api.delete( `/tag/${ tag.id }` ).then();
+
+                expect( typeof body ).toBe( 'object' );
+            });
+
+            it( '.status equals "success"', async () =>
+            {
+                const { body: { status } } = await endpoint.api.delete( `/tag/${ tag.id }` ).then();
+
+                expect( status ).toBe( 'success' );
+            });
+
+            it( '.content equals id of deleted tag', async () =>
+            {
+                const { body: { content } } = await endpoint.api.delete( `/tag/${ tag.id }` ).then();
+
+                expect( content ).toBe( tag.id );
+            });
+
+        });
+
+    });
+
+});
