a3a270f03036aec2d36358e9ae586a2191501e5b
SalathielGenese <salathielgenese@gmail.com>
2019-03-12 03:53:01 +0100

    spec GET /tag/{id} respond HTTP 404 if not found

---
+++ spec/integration/tag.get-by-id.spec.js
@@ -0,0 +1,31 @@
+import { sequelize } from '../../models/index';
+import { endpoint } from '../endpoint';
+import uuid from 'uuid';
+
+
+
+describe( 'GET /tag/{id}', () =>
+{
+    beforeEach( async () =>
+    {
+        label = uuid();
+        tag = await Tag.create({ label });
+    });
+
+    afterEach( async () =>
+    {
+        await tag.destroy();
+    })
+
+    let tag;
+    let label;
+    const Tag = sequelize.model( 'Tag' );
+
+    it( 'respond HTTP 404 if not found', async () =>
+    {
+        const { status } = await endpoint.api.get( `/tag/${ tag.id + 1E7 }` ).then( void 0, reason => reason );
+
+        expect( status ).toBe( 404 );
+    });
+
+});
